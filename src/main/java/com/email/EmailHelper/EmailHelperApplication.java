a06c5b8754d7b4311f33676cf1ba0fff7c4c4e1b
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2025-02-12 02:03:00 +0530

    Initial commit

---
+++ src/main/java/com/email/EmailHelper/EmailHelperApplication.java
@@ -0,0 +1,14 @@
+package com.email.EmailHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailHelperApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		SpringApplication.run(EmailHelperApplication.class, args);
+	}
+
+}
